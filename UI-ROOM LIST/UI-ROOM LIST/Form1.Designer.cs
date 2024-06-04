bfa60f7ee560dfcfec41dd0846eefdf2b3601de1
yengyeng <162948785+yshaprz@users.noreply.github.com>
2024-06-04 14:45:04 +0800

    Add files via upload

---
+++ UI-ROOM LIST/UI-ROOM LIST/Form1.Designer.cs
@@ -0,0 +1,445 @@
+﻿namespace UI_ROOM_LIST
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Panel panel3;
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            panel6 = new Panel();
+            label6 = new Label();
+            label7 = new Label();
+            panel5 = new Panel();
+            label4 = new Label();
+            label5 = new Label();
+            pictureBox5 = new PictureBox();
+            pictureBox4 = new PictureBox();
+            panel4 = new Panel();
+            label3 = new Label();
+            label2 = new Label();
+            pictureBox3 = new PictureBox();
+            panel1 = new Panel();
+            button7 = new Button();
+            button6 = new Button();
+            button5 = new Button();
+            button4 = new Button();
+            button3 = new Button();
+            button2 = new Button();
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            panel2 = new Panel();
+            button8 = new Button();
+            label1 = new Label();
+            pictureBox2 = new PictureBox();
+            panel3 = new Panel();
+            panel3.SuspendLayout();
+            panel6.SuspendLayout();
+            panel5.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).BeginInit();
+            panel4.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).BeginInit();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            SuspendLayout();
+            // 
+            // panel3
+            // 
+            panel3.BackColor = Color.LightGray;
+            panel3.Controls.Add(panel6);
+            panel3.Controls.Add(panel5);
+            panel3.Controls.Add(pictureBox5);
+            panel3.Controls.Add(pictureBox4);
+            panel3.Controls.Add(panel4);
+            panel3.Controls.Add(pictureBox3);
+            panel3.Location = new Point(44, 301);
+            panel3.Name = "panel3";
+            panel3.Size = new Size(972, 269);
+            panel3.TabIndex = 4;
+            panel3.Paint += panel3_Paint;
+            // 
+            // panel6
+            // 
+            panel6.BackColor = Color.FromArgb(0, 7, 45);
+            panel6.Controls.Add(label6);
+            panel6.Controls.Add(label7);
+            panel6.Location = new Point(675, 193);
+            panel6.Name = "panel6";
+            panel6.Size = new Size(258, 63);
+            panel6.TabIndex = 9;
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.Transparent;
+            label6.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label6.ForeColor = Color.White;
+            label6.Location = new Point(39, 38);
+            label6.Name = "label6";
+            label6.Size = new Size(193, 16);
+            label6.TabIndex = 6;
+            label6.Text = "2-4 adults | Standard Amenities ";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.BackColor = Color.Transparent;
+            label7.Font = new Font("Microsoft Sans Serif", 20.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label7.ForeColor = Color.White;
+            label7.Location = new Point(-5, 1);
+            label7.Name = "label7";
+            label7.Size = new Size(269, 31);
+            label7.TabIndex = 5;
+            label7.Text = "STANDARD ROOM";
+            // 
+            // panel5
+            // 
+            panel5.BackColor = Color.FromArgb(0, 7, 45);
+            panel5.Controls.Add(label4);
+            panel5.Controls.Add(label5);
+            panel5.Location = new Point(357, 193);
+            panel5.Name = "panel5";
+            panel5.Size = new Size(258, 63);
+            panel5.TabIndex = 7;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.BackColor = Color.Transparent;
+            label4.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label4.ForeColor = Color.White;
+            label4.Location = new Point(39, 38);
+            label4.Name = "label4";
+            label4.Size = new Size(196, 16);
+            label4.TabIndex = 6;
+            label4.Text = "2-3 adults | Executive Amenities ";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.BackColor = Color.Transparent;
+            label5.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label5.ForeColor = Color.White;
+            label5.Location = new Point(16, 1);
+            label5.Name = "label5";
+            label5.Size = new Size(227, 37);
+            label5.TabIndex = 5;
+            label5.Text = "SUITE ROOM";
+            // 
+            // pictureBox5
+            // 
+            pictureBox5.BackgroundImage = (Image)resources.GetObject("pictureBox5.BackgroundImage");
+            pictureBox5.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox5.Location = new Point(675, 13);
+            pictureBox5.Name = "pictureBox5";
+            pictureBox5.Size = new Size(258, 180);
+            pictureBox5.TabIndex = 8;
+            pictureBox5.TabStop = false;
+            // 
+            // pictureBox4
+            // 
+            pictureBox4.BackgroundImage = (Image)resources.GetObject("pictureBox4.BackgroundImage");
+            pictureBox4.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox4.Location = new Point(357, 13);
+            pictureBox4.Name = "pictureBox4";
+            pictureBox4.Size = new Size(258, 180);
+            pictureBox4.TabIndex = 5;
+            pictureBox4.TabStop = false;
+            pictureBox4.Click += pictureBox4_Click;
+            // 
+            // panel4
+            // 
+            panel4.BackColor = Color.FromArgb(0, 7, 45);
+            panel4.Controls.Add(label3);
+            panel4.Controls.Add(label2);
+            panel4.Location = new Point(36, 193);
+            panel4.Name = "panel4";
+            panel4.Size = new Size(258, 63);
+            panel4.TabIndex = 4;
+            panel4.Paint += panel4_Paint;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.BackColor = Color.Transparent;
+            label3.Font = new Font("Microsoft Sans Serif", 9.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            label3.ForeColor = Color.White;
+            label3.Location = new Point(39, 38);
+            label3.Name = "label3";
+            label3.Size = new Size(180, 16);
+            label3.TabIndex = 6;
+            label3.Text = "2-3 adults | Deluxe Amenities ";
+            label3.Click += label3_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.BackColor = Color.Transparent;
+            label2.Font = new Font("Microsoft Sans Serif", 24F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label2.ForeColor = Color.White;
+            label2.Location = new Point(-1, 1);
+            label2.Name = "label2";
+            label2.Size = new Size(272, 37);
+            label2.TabIndex = 5;
+            label2.Text = "DELUXE ROOM ";
+            // 
+            // pictureBox3
+            // 
+            pictureBox3.BackgroundImage = (Image)resources.GetObject("pictureBox3.BackgroundImage");
+            pictureBox3.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox3.Location = new Point(36, 13);
+            pictureBox3.Name = "pictureBox3";
+            pictureBox3.Size = new Size(258, 180);
+            pictureBox3.TabIndex = 3;
+            pictureBox3.TabStop = false;
+            // 
+            // panel1
+            // 
+            panel1.BackColor = Color.FromArgb(0, 7, 45);
+            panel1.Controls.Add(button7);
+            panel1.Controls.Add(button6);
+            panel1.Controls.Add(button5);
+            panel1.Controls.Add(button4);
+            panel1.Controls.Add(button3);
+            panel1.Controls.Add(button2);
+            panel1.Controls.Add(button1);
+            panel1.Controls.Add(pictureBox1);
+            panel1.Location = new Point(-1, 0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(233, 659);
+            panel1.TabIndex = 0;
+            // 
+            // button7
+            // 
+            button7.BackgroundImage = (Image)resources.GetObject("button7.BackgroundImage");
+            button7.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Location = new Point(19, 563);
+            button7.Name = "button7";
+            button7.Size = new Size(203, 62);
+            button7.TabIndex = 7;
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.BackgroundImage = (Image)resources.GetObject("button6.BackgroundImage");
+            button6.BackgroundImageLayout = ImageLayout.Stretch;
+            button6.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Location = new Point(5, 454);
+            button6.Name = "button6";
+            button6.Size = new Size(203, 45);
+            button6.TabIndex = 6;
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.BackgroundImage = (Image)resources.GetObject("button5.BackgroundImage");
+            button5.BackgroundImageLayout = ImageLayout.Stretch;
+            button5.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Location = new Point(9, 387);
+            button5.Name = "button5";
+            button5.Size = new Size(203, 45);
+            button5.TabIndex = 5;
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.BackgroundImage = (Image)resources.GetObject("button4.BackgroundImage");
+            button4.BackgroundImageLayout = ImageLayout.Stretch;
+            button4.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Location = new Point(12, 321);
+            button4.Name = "button4";
+            button4.Size = new Size(203, 45);
+            button4.TabIndex = 4;
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.BackgroundImage = (Image)resources.GetObject("button3.BackgroundImage");
+            button3.BackgroundImageLayout = ImageLayout.Stretch;
+            button3.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Location = new Point(2, 255);
+            button3.Name = "button3";
+            button3.Size = new Size(202, 45);
+            button3.TabIndex = 3;
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.BackgroundImageLayout = ImageLayout.Stretch;
+            button2.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button2.FlatStyle = FlatStyle.Flat;
+            button2.Location = new Point(13, 188);
+            button2.Name = "button2";
+            button2.Size = new Size(203, 45);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Stretch;
+            button1.FlatAppearance.BorderColor = Color.FromArgb(0, 7, 45);
+            button1.FlatStyle = FlatStyle.Flat;
+            button1.Location = new Point(13, 126);
+            button1.Name = "button1";
+            button1.Size = new Size(203, 45);
+            button1.TabIndex = 1;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackgroundImage = (Image)resources.GetObject("pictureBox1.BackgroundImage");
+            pictureBox1.BackgroundImageLayout = ImageLayout.Zoom;
+            pictureBox1.Location = new Point(8, 5);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(217, 75);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            panel2.BackgroundImage = (Image)resources.GetObject("panel2.BackgroundImage");
+            panel2.BackgroundImageLayout = ImageLayout.Stretch;
+            panel2.Controls.Add(button8);
+            panel2.Controls.Add(panel3);
+            panel2.Controls.Add(label1);
+            panel2.Controls.Add(pictureBox2);
+            panel2.Location = new Point(223, 0);
+            panel2.Name = "panel2";
+            panel2.Size = new Size(1064, 659);
+            panel2.TabIndex = 1;
+            panel2.Paint += panel2_Paint_1;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(0, 7, 45);
+            button8.FlatAppearance.BorderColor = Color.White;
+            button8.FlatAppearance.BorderSize = 2;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Microsoft Sans Serif", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            button8.ForeColor = Color.White;
+            button8.Location = new Point(409, 590);
+            button8.Name = "button8";
+            button8.Size = new Size(242, 38);
+            button8.TabIndex = 5;
+            button8.Text = "Go to Room List ";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.BackColor = Color.Transparent;
+            label1.Font = new Font("Microsoft Sans Serif", 45F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = Color.White;
+            label1.Location = new Point(308, 236);
+            label1.Name = "label1";
+            label1.Size = new Size(452, 69);
+            label1.TabIndex = 2;
+            label1.Text = "Rooms offered:";
+            label1.Click += label1_Click;
+            // 
+            // pictureBox2
+            // 
+            pictureBox2.BackColor = Color.Transparent;
+            pictureBox2.BackgroundImage = (Image)resources.GetObject("pictureBox2.BackgroundImage");
+            pictureBox2.BackgroundImageLayout = ImageLayout.Stretch;
+            pictureBox2.Location = new Point(440, 121);
+            pictureBox2.Name = "pictureBox2";
+            pictureBox2.Size = new Size(203, 93);
+            pictureBox2.TabIndex = 0;
+            pictureBox2.TabStop = false;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(1284, 658);
+            Controls.Add(panel2);
+            Controls.Add(panel1);
+            Font = new Font("Microsoft Sans Serif", 9F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            Name = "Form1";
+            Text = "ROOMS ";
+            Load += Form1_Load;
+            panel3.ResumeLayout(false);
+            panel6.ResumeLayout(false);
+            panel6.PerformLayout();
+            panel5.ResumeLayout(false);
+            panel5.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox5).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBox4).EndInit();
+            panel4.ResumeLayout(false);
+            panel4.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox3).EndInit();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            panel2.ResumeLayout(false);
+            panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private PictureBox pictureBox1;
+        private Button button1;
+        private Button button2;
+        private Button button5;
+        private Button button4;
+        private Button button3;
+        private Button button6;
+        private Button button7;
+        private Panel panel2;
+        private PictureBox pictureBox2;
+        private Label label1;
+        private Panel panel3;
+        private PictureBox pictureBox3;
+        private Panel panel4;
+        private Label label2;
+        private Label label3;
+        private Panel panel6;
+        private Label label6;
+        private Label label7;
+        private Panel panel5;
+        private Label label4;
+        private Label label5;
+        private PictureBox pictureBox5;
+        private PictureBox pictureBox4;
+        private Button button8;
+    }
+}
---
+++ UI-ROOM LIST/UI-ROOM LIST/Form1.cs
@@ -0,0 +1,60 @@
+namespace UI_ROOM_LIST
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint_1(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
